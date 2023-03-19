9a222c7a85f8bd5fac7f082126d99f0888a8cb78
Miftjnnh <115403680+Miftjnnh@users.noreply.github.com>
2023-03-19 21:40:49 +0700

    Add files via upload

---
+++ praktikum04/function.php
@@ -0,0 +1,12 @@
+<?php
+include "library.php";
+
+sayHello("Mita");
+
+echo "<br>";
+
+echo hitungPersegi(20);
+
+echo "<br>";
+
+echo "ini function.php";
---
+++ praktikum04/projek/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php require_once "components/head.php" ?>
+    </head>
+    <body>
+        <div class="d-flex" id="wrapper">
+            <!-- Sidebar-->
+            <?php include_once "components/side.php" ?>
+            <!-- end of side -->
+
+            <!-- Page content wrapper-->
+            <div id="page-content-wrapper">
+                <!-- Top navigation-->
+            
+            <!-- Navbar -->
+            <?php include_once "components/navbar.php" ?>
+            <!-- end of navbar -->
+
+                <!-- Page content-->
+                <div class="container-fluid">
+                    <h1 class="mt-4">Tentang e-Ruang</h1>
+                    <p>e-Ruang berbasis website aplikasi memberikan kemudahan dan kenyamanan untuk seluruh civitas akademika STT Terpadu NUrul Fikri untuk meminjam fasilitad-fasilitas dan ruangan dalam kampus.</p>
+                    <p>
+                        e-Ruang dapat memproses booking atau pinjaman fasilitas secara online. Peminjaman fasilitas kampus dapat dipinjam oleh seluruh civitas akademika STT Terpadu Nurul Fikri secara online pada website ini. Fasilitas kampus dapat disewa untuk kegiatan belajar, praktikum, atau kegiatan yang dilakukan adalah kegiatan akademik maupun non akademik.
+                        
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
